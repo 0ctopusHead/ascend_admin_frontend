f853c777f0837b85b03cd3e1814709710ed5e977
0ctopusHead <yanwarut_s@cmu.ac.th>
2024-06-11 15:56:26 +0700

    initial commit

---
+++ src/components/DropZone.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="dropzone">
+    <span>Drag or Drop File</span>
+    <span>or</span>
+    <label for="dropzoneFile">Select File</label>
+    <input type="file" id="dropzoneFile" />
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style>
+.dropzone {
+  width: 400px;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 16px;
+  border: 2px dashed #41b883;
+  background-color: black;
+  transition: 0.3s ease all;
+}
+label {
+  padding: 8px 12px;
+  color: #fff;
+  background-color: #41b883;
+  transition: 0.3s ease all;
+}
+input {
+  display: none;
+}
+</style>
---
+++ src/views/UploadView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <h1>Upload</h1>
+    <DropZone></DropZone>
+    <span class="file-info">File: {{ dropzoneFile }}</span>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+import DropZone from '../components/DropZone.vue'
+
+let dropzoneFile = ref('')
+</script>
