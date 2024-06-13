6b2e40303ad36692b8b718a0247eeab42750e3f3
0ctopusHead <yanwarut_s@cmu.ac.th>
2024-06-13 13:24:11 +0700

    added the logo, user icon, logout icon to navbar

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,18 @@ import './assets/style.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+library.add(faUser, faRightFromBracket)
 
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(createPinia())
 app.use(router)
 
