7cc0fc120a2585327938e95ebd1d8c2c75513bdd
0ctopusHead <yanwarut_s@cmu.ac.th>
2024-06-14 18:06:33 +0700

    create concrete background component

---
+++ src/components/ConcreteZoneComponent.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import type { PropType } from 'vue'
+const props = defineProps({
+  label: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+<template>
+  <div class="relative h-screen">
+    <div class="absolute inset-0 flex items-center justify-center">
+      <div class="bg-white w-5/6 h-5/6 p-4 shadow-lg rounded-lg">
+        <h1 v-if="label">{{ label }}</h1>
+      </div>
+    </div>
+  </div>
+</template>
